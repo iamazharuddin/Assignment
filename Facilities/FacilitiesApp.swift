40649ec79e0182310a8b6577cd3e342e36531f72
iamazharuddin <iamazharuddin10@gmail.com>
2023-06-28 19:48:43 +0530

    Initial Commit

---
+++ Facilities/FacilitiesApp.swift
@@ -0,0 +1,17 @@
+//
+//  FacilitiesApp.swift
+//  Facilities
+//
+//  Created by Azharuddin 1 on 28/06/23.
+//
+
+import SwiftUI
+
+@main
+struct FacilitiesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
